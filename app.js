30cf19b98027a0ecc5ce662ff740244bc6e04f8e
fershoot979 <145419450+fershoot979@users.noreply.github.com>
2025-05-07 23:55:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIIIIIIZ', 'CUMPLEEEEE','BONITA♥'],
+			strings: [ 'FELIIIZ', 'CUMPLEEEE','BONITA♥'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
